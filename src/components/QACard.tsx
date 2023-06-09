6ea02c9323a6f9d566dc72f84551f0cca10bcb3d
haytham918 <yunxuant@umich.edu>
2023-06-09 11:29:26 -0400

    Adding translateZ(3px) for the stuff in the back to fix bug on IOS

--- src/components/QACard.tsx
+++ src/components/QACard.tsx
@@ -1,12 +1,18 @@
 import React, { useEffect, useRef, useState } from "react";
-import { IonCard, IonCardContent, IonText, createGesture, IonItem, IonIcon } from "@ionic/react";
+import {
+  IonCard,
+  IonCardContent,
+  IonText,
+  createGesture,
+  IonItem,
+  IonIcon,
+} from "@ionic/react";
 import "./QACard.css";
-import {sadOutline, happy, card, close} from 'ionicons/icons'
-const QACard: React.FC<{ obj: flashCard}> = (props) => {
+import { sadOutline, happy, card, close } from "ionicons/icons";
+const QACard: React.FC<{ obj: flashCard }> = (props) => {
   const [isClicked, setIsClicked] = useState(false);
-
   const style = isClicked
-    ? { transform: "rotateY(180deg)", backgroundColor: "rgba(251,255,236,1)" }
+    ? { transform: "rotateY(180deg)", background: "rgba(251,255,236,1)" }
     : { transform: "rotateY(0deg)" };
   const clickHandler = () => {
     setIsClicked(!isClicked);
@@ -25,14 +31,9 @@ const QACard: React.FC<{ obj: flashCard}> = (props) => {
   const [onemoreOpacity, setOneMoreOp] = useState(0);
   const [nomoreOpacity, setNoMoreOp] = useState(0);
 
-  console.log('neg', negativeOpacity);
-  console.log('pos', positiveOpacity);
-  console.log('onemore', onemoreOpacity);
-  console.log('nomore', nomoreOpacity);
-
   const initGesture = () => {
     const card = ref.current;
-   
+
     if (card) {
       const gestureX = createGesture({
         el: card,
@@ -42,11 +43,10 @@ const QACard: React.FC<{ obj: flashCard}> = (props) => {
           card.style.transform = `translateX(${detail.deltaX}px) rotate(${
             detail.deltaX / 20
           }deg)`;
-          if(detail.deltaX > 0){
+          if (detail.deltaX > 0) {
             setNegOp(0);
             setPosOp(detail.deltaX / 250);
-            
-          }else{
+          } else {
             setPosOp(0);
             setNegOp(-detail.deltaX / 250);
           }
@@ -58,8 +58,7 @@ const QACard: React.FC<{ obj: flashCard}> = (props) => {
           if (detail.deltaX > windowWidth / 2) {
             card.style.transform = `translateX(${windowWidth * 1.5}px)`;
           } else if (detail.deltaX < -windowWidth / 2) {
-           card.style.transform = `translateX(${-windowWidth * 1.5}px)`;
-           
+            card.style.transform = `translateX(${-windowWidth * 1.5}px)`;
           } else {
             card.style.transform = "";
             setNegOp(0);
@@ -74,10 +73,10 @@ const QACard: React.FC<{ obj: flashCard}> = (props) => {
         gestureName: "swipe-y",
         onMove: (detail) => {
           card.style.transform = `translateY(${detail.deltaY}px)`;
-          if(detail.deltaY > 0){
+          if (detail.deltaY > 0) {
             setOneMoreOp(0);
             setNoMoreOp(detail.deltaY / 200);
-          }else{
+          } else {
             setNoMoreOp(0);
             setOneMoreOp(-detail.deltaY / 200);
           }
@@ -112,76 +111,114 @@ const QACard: React.FC<{ obj: flashCard}> = (props) => {
         style={{ height: "100%", display: "flex" }}
         onClick={clickHandler}
       >
-        
         <IonCardContent class="card-content" style={style}>
-
           {/* //Front Negative */}
-         <div className="front negative front-negative" style={{opacity: negativeOpacity}}>
-          <IonItem className='action-container negative-container'>
-            <IonText className="action-text negative-text">F O R G E T </IonText>
-            <IonIcon icon={sadOutline} style={{color: '#D63230', fontSize: '2em'}}></IonIcon>
-          </IonItem>
-         </div>
+          <div
+            className="front negative front-negative"
+            style={{ opacity: negativeOpacity }}
+          >
+            <IonItem className="action-container negative-container">
+              <IonText className="action-text negative-text">
+                F O R G E T{" "}
+              </IonText>
+              <IonIcon
+                icon={sadOutline}
+                style={{ color: "#D63230", fontSize: "2em" }}
+              ></IonIcon>
+            </IonItem>
+          </div>
 
-        {/* Front Positive */}
-         <div className="front positive front-positive" style={{opacity: positiveOpacity}}>
-          <IonItem className='action-container positive-container'>
-            <IonText className="action-text positive-text">K N O W</IonText>
-            <IonIcon icon={happy} style={{color: '#256D1B', fontSize: '2em'}}></IonIcon>
-          </IonItem>
-         </div>
+          {/* Front Positive */}
+          <div
+            className="front positive front-positive"
+            style={{ opacity: positiveOpacity }}
+          >
+            <IonItem className="action-container positive-container">
+              <IonText className="action-text positive-text">K N O W</IonText>
+              <IonIcon
+                icon={happy}
+                style={{ color: "#256D1B", fontSize: "2em" }}
+              ></IonIcon>
+            </IonItem>
+          </div>
 
-        {/* Front OneMore */}
-         <div className="front onemore" style={{opacity: onemoreOpacity}}>
-          <IonItem className='action-container onemore-container'>
-            <IonText className="action-text onemore-text">ONE MORE</IonText>
-            <IonIcon icon={card} style={{color: '#7BB2D9', fontSize: '2em'}}></IonIcon>
-          </IonItem>
-         </div>
+          {/* Front OneMore */}
+          <div className="front onemore" style={{ opacity: onemoreOpacity }}>
+            <IonItem className="action-container onemore-container">
+              <IonText className="action-text onemore-text">ONE MORE</IonText>
+              <IonIcon
+                icon={card}
+                style={{ color: "#7BB2D9", fontSize: "2em" }}
+              ></IonIcon>
+            </IonItem>
+          </div>
 
-        {/* Front Nomore */}
-         <div className="front nomore" style={{opacity: nomoreOpacity}}>
-          <IonItem className='action-container nomore-container'>
-            <IonText className="action-text nomore-text">NO MORE</IonText>
-            <IonIcon icon={close} style={{color: '#E8871E', fontSize: '2em'}}></IonIcon>
-          </IonItem>
-         </div>
+          {/* Front Nomore */}
+          <div className="front nomore" style={{ opacity: nomoreOpacity }}>
+            <IonItem className="action-container nomore-container">
+              <IonText className="action-text nomore-text">NO MORE</IonText>
+              <IonIcon
+                icon={close}
+                style={{ color: "#E8871E", fontSize: "2em" }}
+              ></IonIcon>
+            </IonItem>
+          </div>
 
           <IonText class="question-text">{question}</IonText>
+
           <IonText class="answer-text">{answer}</IonText>
 
-        {/* Back Negative */}
-        <div className="back negative back-negative" style={{opacity: negativeOpacity}}>
-          <IonItem className='action-container negative-container'>
-            <IonText className="action-text negative-text">F O R G E T </IonText>
-            <IonIcon icon={sadOutline} style={{color: '#D63230', fontSize: '2em'}}></IonIcon>
-          </IonItem>
-        </div>
+          {/* Back Negative */}
+          <div
+            className="back negative back-negative"
+            style={{ opacity: negativeOpacity }}
+          >
+            <IonItem className="action-container negative-container">
+              <IonText className="action-text negative-text">
+                F O R G E T{" "}
+              </IonText>
+              <IonIcon
+                icon={sadOutline}
+                style={{ color: "#D63230", fontSize: "2em" }}
+              ></IonIcon>
+            </IonItem>
+          </div>
 
-        {/* Back Positive */}
-        <div className="back positive back-positive" style={{opacity: positiveOpacity}}>
-          <IonItem className='action-container positive-container'>
-            <IonText className="action-text positive-text">K N O W</IonText>
-            <IonIcon icon={happy} style={{color: '#256D1B', fontSize: '2em'}}></IonIcon>
-          </IonItem>
-         </div>
+          {/* Back Positive */}
+          <div
+            className="back positive back-positive"
+            style={{ opacity: positiveOpacity }}
+          >
+            <IonItem className="action-container positive-container">
+              <IonText className="action-text positive-text">K N O W</IonText>
+              <IonIcon
+                icon={happy}
+                style={{ color: "#256D1B", fontSize: "2em" }}
+              ></IonIcon>
+            </IonItem>
+          </div>
 
-         {/* Back OneMore */}
-         <div className="back onemore" style={{opacity: onemoreOpacity}}>
-          <IonItem className='action-container onemore-container'>
-            <IonText className="action-text onemore-text">ONE MORE</IonText>
-            <IonIcon icon={card} style={{color: '#7BB2D9', fontSize: '2em'}}></IonIcon>
-          </IonItem>
-         </div>
+          {/* Back OneMore */}
+          <div className="back onemore" style={{ opacity: onemoreOpacity }}>
+            <IonItem className="action-container onemore-container">
+              <IonText className="action-text onemore-text">ONE MORE</IonText>
+              <IonIcon
+                icon={card}
+                style={{ color: "#7BB2D9", fontSize: "2em" }}
+              ></IonIcon>
+            </IonItem>
+          </div>
 
           {/* Back NoMore */}
-         <div className="back nomore" style={{opacity: nomoreOpacity}}>
-          <IonItem className='action-container nomore-container'>
-            <IonText className="action-text nomore-text">NO MORE</IonText>
-            <IonIcon icon={close} style={{color: '#E8871E', fontSize: '2em'}}></IonIcon>
-          </IonItem>
-         </div>
-
+          <div className="back nomore" style={{ opacity: nomoreOpacity }}>
+            <IonItem className="action-container nomore-container">
+              <IonText className="action-text nomore-text">NO MORE</IonText>
+              <IonIcon
+                icon={close}
+                style={{ color: "#E8871E", fontSize: "2em" }}
+              ></IonIcon>
+            </IonItem>
+          </div>
         </IonCardContent>
       </IonCard>
     </div>
