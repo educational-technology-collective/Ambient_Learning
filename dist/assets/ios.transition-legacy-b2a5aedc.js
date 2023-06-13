e68484d4cd90e6e7665f7a6a8a1822d555db8fd1
haytham918 <yunxuant@umich.edu>
2023-06-13 11:45:58 -0400

    Deployed on Android and Fix Button All Caps Bug

--- dist/assets/ios.transition-legacy-b2a5aedc.js
+++ dist/assets/ios.transition-legacy-b2a5aedc.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-5079015f.js"],(function(e,t){"use strict";var o,n;return{setters:[e=>{o=e.l,n=e.m}],execute:function(){
+System.register(["./index-legacy-3deada68.js"],(function(e,t){"use strict";var o,n;return{setters:[e=>{o=e.l,n=e.m}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
--- dist/assets/swipe-back-legacy-e5416071.js
+++ dist/assets/swipe-back-legacy-e5416071.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-5079015f.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-3deada68.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
