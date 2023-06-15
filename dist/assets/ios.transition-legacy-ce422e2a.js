886ba766e6a2aa2d260da94012bb2cb45707402d
haytham918 <yunxuant@umich.edu>
2023-06-15 17:05:27 -0400

    Minor change

--- dist/assets/ios.transition-legacy-ce422e2a.js
+++ dist/assets/ios.transition-legacy-ce422e2a.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-e1d0440b.js"],(function(e,t){"use strict";var o,n;return{setters:[e=>{o=e.l,n=e.m}],execute:function(){
+System.register(["./index-legacy-9319735a.js"],(function(e,t){"use strict";var o,n;return{setters:[e=>{o=e.l,n=e.m}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
--- dist/assets/swipe-back-legacy-52fdd211.js
+++ dist/assets/swipe-back-legacy-52fdd211.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-e1d0440b.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-9319735a.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
