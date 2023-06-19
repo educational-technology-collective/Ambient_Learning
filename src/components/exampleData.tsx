f8610628027d5c14e6b9f209611fe4cd582b4a95
haytham918 <yunxuant@umich.edu>
2023-06-19 14:12:45 -0400

    Remove timestamp info

--- src/components/exampleData.tsx
+++ src/components/exampleData.tsx
@@ -5,7 +5,6 @@ export const cardCollection = [
     index: 1, // Used For Key
     type: "q", // Identify Type of Card MCQ/QA
     seenTimes: 1, // **Used for Algorithm
-    timeStamp: "2:03", // Not Sure Yet
     timeInterval: 4, // ** Used For Algorithm
 
     // This is the only thing displays
@@ -20,7 +19,6 @@ export const cardCollection = [
     index: 2, //Used For Key
     type: "m", //Identify Type of Card MCQ/QA
     seenTimes: 2, // **Used for Algorithm
-    timeStamp: "20: 14", // Not sure yet
     timeInterval: 6,// ** Used for Algorithm
 
     //This is the only thing Displays
@@ -40,7 +38,6 @@ export const cardCollection = [
     index: 4,
     type: "q",
     seenTimes: 2,
-    timeStamp: "12:10",
     timeInterval: 4,
     content: {
       question: "Write Code to increment variable x by 2",
@@ -52,7 +49,6 @@ export const cardCollection = [
     index: 9,
     type: "m",
     seenTimes: 1,
-    timeStamp: "11:11",
     timeInterval: 4,
     content: {
       question: "Extract 'word' from x = 'whatawordcool'",
@@ -72,7 +68,6 @@ export const cardCollection = [
     index: 7,
     type: "q",
     seenTimes: 2,
-    timeStamp: "31:10",
     timeInterval: 8,
     content: {
       question: "What type of statements are If/Else called",
@@ -84,7 +79,6 @@ export const cardCollection = [
     index: 5,
     type: "m",
     seenTimes: 2,
-    timeStamp: "8: 12",
     timeInterval: 3,
     content: {
       question: "Negate the boolean variable isRight",
@@ -101,7 +95,6 @@ export const cardCollection = [
     index: 12,
     type: "q",
     seenTimes: 3,
-    timeStamp: "12: 12",
     timeInterval: 4,
     content: {
       question: "x = x + 1 is equivalent to x += 1",
@@ -113,7 +106,6 @@ export const cardCollection = [
     index: 45,
     type: "q",
     seenTimes: 3,
-    timeStamp: "12:08",
     timeInterval: 5,
     content: {
       question: "Describe what is happening:\ndef func():\n\t func()",
@@ -125,7 +117,6 @@ export const cardCollection = [
     index: 31,
     type: "m",
     seenTime: 5,
-    timeStamp: "08:09",
     timeInterval: 3,
     content: {
       question: 'What is the output of:\n print("12" + 13)',
@@ -145,7 +136,6 @@ export const cardCollection = [
     index: 25,
     type: "m",
     seenTime: 10,
-    timeStamp: "04: 12",
     timeInterval: 10,
     content: {
       question: "answer = [15, 28, 19, 20, 45]\nWhat is answer[-1]",
@@ -161,3 +151,9 @@ export const cardCollection = [
     },
   },
 ];
+
+const backendCollection = [
+  {
+
+  }
+]
