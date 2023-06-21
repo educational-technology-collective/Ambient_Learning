9331ec3aa98fd5998395f027c6afb2f47df75648
haytham918 <yunxuant@umich.edu>
2023-06-21 14:45:52 -0400

    Stress Test and bug fix

--- dist/assets/swipe-back-legacy-b829ee8c.js
+++ dist/assets/swipe-back-legacy-b829ee8c.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-695719f0.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-bbd442b7.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
