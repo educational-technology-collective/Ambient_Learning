bdb46cb27f33a7ba82abcd508efce68745fd4b7a
haytham918 <yunxuant@umich.edu>
2023-06-23 14:29:33 -0400

    Adjust the cards

--- dist/assets/swipe-back-legacy-5ab8517a.js
+++ dist/assets/swipe-back-legacy-5ab8517a.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-fd8332e9.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-9421cc5c.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
