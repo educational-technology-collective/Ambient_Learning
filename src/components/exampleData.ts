e67857c9bffcf0fa6f3ee075807d5fd94c4cce0a
haytham918 <yunxuant@umich.edu>
2023-06-26 14:37:40 -0400

    Test with some of Mai's cards

--- src/components/exampleData.ts
+++ src/components/exampleData.ts
@@ -49,85 +49,91 @@ export const cardCollection = [
 
   [
     {
-      id: 3210,
-      type: "q",
+      id: 23132,
+      type: "m",
       content: {
-        question: "Write Code to multiply variable x by 3",
-        answer: "x *= 3",
-      },
-    },
+          question: "Which of the following regex patterns will correctly match words starting with a capital letter?",
+          answer: [
+              {option: "r'[A-Za-z]+'",
+               isCorrect: false},
+              {option: "r'[A-Z][a-z]*'",
+               isCorrect: true},
+              {option: "r'[a-zA-Z]+'",
+               isCorrect: false},
+              {option: "r'[a-z][A-Z]+'",
+               isCorrect: false}
+          ]
+      }
+  },
+  {
+   id: 4,
+    type: "m",
+    content: {
+        question: "Which of the following regex patterns will correctly match names of students who received a B grade?",
+        answer: [
+            {option: "r'\w+\s\w+(?=: B)'",
+             isCorrect: true},
+            {option: "r'\w+\s\w+(?= B)'",
+             isCorrect: false},
+            {option: "r'\w+\s\w+'",
+             isCorrect: false},
+            {option: "r': B'",
+             isCorrect: false}
+        ]
+    }
+}],
+
+  [
     {
-      id: 4,
+      id: 44124,
       type: "q",
       content: {
-        question: "Write Code to increment variable x by 2",
-        answer: "x += 2",
-      },
-    },
+          question: "How can you filter a DataFrame based on a specific column value in Pandas?",
+          answer: "To filter a DataFrame based on a specific column value, use the syntax `df[df['column_name'] == value]`, where 'df' is the DataFrame, 'column_name' is the name of the column, and 'value' is the desired value to filter by."
+      }
+  },
+    {
+      id: 12313,
+      type: "m",
+      content: {
+          question: "Which of the following data structures is most suitable for storing proportions of children with mothers having different education levels?",
+          answer: [
+              {option: "List", isCorrect: false},
+              {option: "Dictionary", isCorrect: true},
+              {option: "Tuple", isCorrect: false},
+              {option: "Set", isCorrect: false}
+          ]
+      }
+  },
   ],
 
   [
     {
-      id: 415,
+      id: 223,
       type: "m",
       content: {
-        question: "x = '34512', x[1:] = ?",
-        answer: [
-          { option: "4", isCorrect: false },
-          { option: "3", isCorrect: false },
-          { option: "45", isCorrect: false },
-          { option: "4512", isCorrect: true },
-        ],
-      },
-    },
+          question: "Which of the following Pandas methods can be used to count occurrences of unique values in a DataFrame column?",
+          answer: [
+              {option: ".count()", isCorrect: false},
+              {option: ".unique()", isCorrect: false},
+              {option: ".value_counts()", isCorrect: true},
+              {option: ".groupby()", isCorrect: false}
+          ]
+      }
+  },
     {
-      id: 465,
+      id: 523,
       type: "m",
       content: {
-        question: "x = '34512', x[1] = ?",
-        answer: [
-          { option: "4", isCorrect: true },
-          { option: "3", isCorrect: false },
-          { option: "45", isCorrect: false },
-          { option: "4512", isCorrect: false },
-        ],
-      },
-    },
-    {
-      id: 9,
-      type: "m",
-      content: {
-        question: "Extract 'word' from x = 'whatawordcool'",
-        answer: [
-          { option: "x[1, 12]", isCorrect: false },
-          {
-            option: "x[5, 9]",
-            isCorrect: true,
-          },
-          { option: "x[5, 8]", isCorrect: false },
-          { option: "x[5]", isCorrect: false },
-        ],
-      },
-    },
-  ],
-
-  [
-    {
-      id: 34,
-      type: "q",
-      content: {
-        question: "What type of statements are For/While called",
-        answer: "Loop",
-      },
-    },
-    {
-      id: 7,
-      type: "q",
-      content: {
-        question: "What type of statements are If/Else called",
-        answer: "Conditional Statements",
-      },
-    },
+          question: "What is the most suitable data structure for storing the ratio of children who contracted chickenpox but were vaccinated against it, separated by sex?",
+          answer: [
+              {option: "List", isCorrect: false},
+              {option: "Dictionary", isCorrect: true},
+              {option: "Tuple", isCorrect: false},
+              {option: "Set", isCorrect: false}
+          ]
+      }
+  }
   ],
   // ***************************
   [
