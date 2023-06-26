161222f3471a0372c3a332eba5dc36917d5bc13c
haytham918 <yunxuant@umich.edu>
2023-06-26 10:00:25 -0400

    Rename and add components to the alert that displays when one more card fails

--- src/pages/CardScreen.tsx
+++ src/pages/CardScreen.tsx
@@ -8,7 +8,7 @@ import {
 import "./CardScreen.css";
 import React from "react";
 import FlashCardList from "../FlashCardComp/FlashCardList";
-import Message from "../components/Message";
+import Message from "../components/OneMoreFailMessage";
 
 const CardScreen: React.FC<{
   finished: number;
