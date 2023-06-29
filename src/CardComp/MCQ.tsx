8cf1381d1c7c4830c3febbfcd61e42c3213d0726
haytham918 <yunxuant@umich.edu>
2023-06-29 16:26:41 -0400

    Remove length

--- src/CardComp/MCQ.tsx
+++ src/CardComp/MCQ.tsx
@@ -1,10 +1,7 @@
-import { IonText } from "@ionic/react";
 import Choices from "../MCQComponents/Choices";
 import "./Card.css";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import remarkGfm from "remark-gfm";
-import rehypeRaw from "rehype-raw";
-import rehypeHighlight from "rehype-highlight/lib";
 
 const MCQ: React.FC<{
   obj: flashCard;
@@ -12,37 +9,37 @@ const MCQ: React.FC<{
   setClickStatus: () => void;
   setCorrectStatus: () => void;
   handleTestEvaluation: (result: string) => void;
-}> = ({ obj, clicked, setClickStatus, setCorrectStatus, handleTestEvaluation }) => {
+}> = ({
+  obj,
+  clicked,
+  setClickStatus,
+  setCorrectStatus,
+  handleTestEvaluation,
+}) => {
   const question: string = obj.content.question;
   const choice: individualChoice[] = obj.content.answer;
 
-  const questionLength: number = question.length;
-  
   let frontQuestionStyle: string, backQuestionStyle: string;
-  // When there are less than or equal to 110 characters: 2em
-  // if (questionLength <= 110) {
-  //   frontQuestionStyle = "card-text front-text mcq-question back-text-2em";
-  //   backQuestionStyle = "card-text  back-text mcq-question back-text-2em";
-  // } 
-  // When there are less than or equal to 190 characters: 1.6em
-  // else if (questionLength <= 190) {
-    frontQuestionStyle = "front-text mcq-question card-text";
-    backQuestionStyle = "card-text  back-text mcq-question";
-  // } 
-  // When there are more than: 1.3em
-  // else {
-  //   frontQuestionStyle = "card-text front-text mcq-question back-text-1d3em";
-  //   backQuestionStyle = "card-text  back-text mcq-question back-text-1d3em";
-  // }
+
+  frontQuestionStyle = "front-text mcq-question card-text";
+  backQuestionStyle = "card-text  back-text mcq-question";
 
   // Component Being Rendered
   return (
     <>
       {/* Question Text Front */}
-      <ReactMarkdown className={frontQuestionStyle} children={question} remarkPlugins={[remarkGfm]}></ReactMarkdown>
+      <ReactMarkdown
+        className={frontQuestionStyle}
+        children={question}
+        remarkPlugins={[remarkGfm]}
+      ></ReactMarkdown>
 
       {/* Question Text Back */}
-      <ReactMarkdown className={backQuestionStyle} children={question} remarkPlugins={[remarkGfm]}></ReactMarkdown>
+      <ReactMarkdown
+        className={backQuestionStyle}
+        children={question}
+        remarkPlugins={[remarkGfm]}
+      ></ReactMarkdown>
 
       {/* Component for all the choices */}
       <Choices
