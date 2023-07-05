aff29053f554d3b8fb5228c551c61f15c978cc47
haytham918 <yunxuant@umich.edu>
2023-07-05 18:06:43 -0400

    Move Tutorial Page Dummy data to another file

--- src/pages/TutorialPage.tsx
+++ src/pages/TutorialPage.tsx
@@ -1,88 +1,44 @@
-import { IonContent, IonPage, useIonViewWillEnter, useIonViewWillLeave } from "@ionic/react";
+import {
+  IonContent,
+  IonPage,
+  useIonViewWillEnter,
+  useIonViewWillLeave,
+} from "@ionic/react";
 import { useState } from "react";
 import { hideBar, showBar } from "../utilities/showTabBar";
-import '../pages/CardScreen.css'
+import "../pages/CardScreen.css";
 import FlashCardList from "../FlashCardComp/FlashCardList";
 import FinishedDisplay from "../TutorialComp/FinishedDisplay";
 import AppNameHeader from "./AppNameHeader";
+import { cards, dummyInfo } from "../utilities/tutorialpagedata";
 
 const TutorialPage: React.FC = () => {
-
-  const cards = [[{
-    _id: 'sdjdiaj',
-    lmid: 's12najdn',
-    type: 'm',
-    content: {
-      question: "Which direction to indicate this card is bad?",
-      answer: [
-        {option: 'Swipe Left', isCorrect: false},
-        {option: 'Swipe Right', isCorrect: false},
-        {option: 'Swipe Up', isCorrect: false},
-        {option: 'Swipe Down', isCorrect: true}
-      ]
-    }
-  }], [{
-    _id: 'ssdajsiodusda',
-    lmid: 'jjsjaodja',
-    type: 'q',
-    content: {
-      question: 'How do I get one more card of the same concept?',
-      answer: 'Simly Swipe Up!!!'
-    }
-  }],[{
-    _id: 'sdj',
-    lmid: 'sdnajdn',
-    type: 'm',
-    content: {
-      question: "What if I don't know the card?",
-      answer: [
-        {option: 'Swipe Left', isCorrect: true},
-        {option: 'Swipe Right', isCorrect: false},
-        {option: 'Swipe Up', isCorrect: false},
-        {option: 'Swipe Down', isCorrect: false}
-      ]
-    }
-  }],[{
-    _id: 'sdaddusda',
-    lmid: 'jjiajsid',
-    type: 'q',
-    content: {
-      question: 'Hello there! Welocome to Tutorial! Tap the card to reveal the answer. Then Swipe!',
-      answer: 'Great Job! Swipe Right to indicate you know the card!!!'
-    }
-  }]]
-
-  const dummyInfo = {
-    user_id: '',
-    start_time: '',
-    end_time: '',
-    number_shake: 0,
-    action_container: []
-  }
-
+  // Hide the bottom tabs for the tutorial page
   useIonViewWillEnter(hideBar);
+
+  // Display the bottom tabs after
   useIonViewWillLeave(showBar);
 
   const [tutorialCounter, setTutorialCounter] = useState(cards.length);
 
   const swipeDummyNext = () => {
-    setTutorialCounter(prevTutorialCounter => prevTutorialCounter - 1);
-  }
+    setTutorialCounter((prevTutorialCounter) => prevTutorialCounter - 1);
+  };
 
-  console.log(tutorialCounter);
-  return(
+  return (
     <IonPage>
-
       {/* Header for the App Name */}
       <AppNameHeader />
 
-
-     <IonContent scrollY={false} className="home-content">
-     <div className='card-stack'>
+      <IonContent scrollY={false} className="home-content">
+        <div className="card-stack">
           {/* We display two tuples at one time */}
           {cards.map((array: flashCard[], index) => {
             // If the tuple is displayed on top
-            if (index === tutorialCounter - 1 || index === tutorialCounter - 2) {
+            if (
+              index === tutorialCounter - 1 ||
+              index === tutorialCounter - 2
+            ) {
               return (
                 <FlashCardList
                   array={array}
@@ -98,12 +54,11 @@ const TutorialPage: React.FC = () => {
               );
             }
           })}
-         
         </div>
         {tutorialCounter === 0 ? <FinishedDisplay isTutorial={true} /> : null}
-     </IonContent>
+      </IonContent>
     </IonPage>
-  )
+  );
 };
 
 export default TutorialPage;
