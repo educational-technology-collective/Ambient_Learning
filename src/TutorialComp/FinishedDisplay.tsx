d79f2be4f0a30767bb6b430dff4957ff2095faf3
haytham918 <yunxuant@umich.edu>
2023-07-05 18:07:24 -0400

    Refactor the Display file

--- src/TutorialComp/FinishedDisplay.tsx
+++ src/TutorialComp/FinishedDisplay.tsx
@@ -1,33 +1,33 @@
 import { useHistory } from "react-router";
 import { IonButton, IonText } from "@ionic/react";
-import './FinishedDisplay.css'
+import "./FinishedDisplay.css";
 
-const FinishedDisplay: React.FC<{isTutorial: boolean}> = ({isTutorial}) => {
+const FinishedDisplay: React.FC<{ isTutorial: boolean }> = ({ isTutorial }) => {
   const history = useHistory();
   let screenText, buttonText, navigateScreen;
 
-  if(isTutorial){
-    screenText = 'You Completed Tutorial!';
-    buttonText = 'Start Reviewing';
+  if (isTutorial) {
+    screenText = "You Completed Tutorial!";
+    buttonText = "Start Reviewing";
     navigateScreen = () => {
-    history.push('/cardscreen');
-    }
-  }else{
-    screenText = 'You Finished Cards Today!!!';
+      history.push("/cardscreen");
+    };
+  } else {
+    screenText = "You Finished Cards Today!!!";
     buttonText = "Return to Home";
     navigateScreen = () => {
-      history.push('/home');
-    }
+      history.push("/home");
+    };
   }
-  return(
-    <div className='finished-container'>
-      <IonText className="compliment-text">
-        Great Job!
-      </IonText>
+  return (
+    <div className="finished-container">
+      <IonText className="compliment-text">Great Job!</IonText>
       <IonText className="compliment-text">{screenText}</IonText>
-      <IonButton className="button-for-home" onClick={navigateScreen}>{buttonText}</IonButton>
+      <IonButton className="button-for-home" onClick={navigateScreen}>
+        {buttonText}
+      </IonButton>
     </div>
-  )
+  );
 };
 
 export default FinishedDisplay;
