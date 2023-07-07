2cb3da772376cd621129406db55bdac937677582
haytham918 <yunxuant@umich.edu>
2023-07-07 17:49:01 -0400

    Adjust the positions of positive and negative indicators

--- dist/assets/keyboard2-legacy-954413cf.js
+++ dist/assets/keyboard2-legacy-954413cf.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-403d5c69.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-6456c41c.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
--- dist/assets/md.transition-legacy-bf1b31ee.js
+++ dist/assets/md.transition-legacy-bf1b31ee.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-403d5c69.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.m,i=e.l}],execute:function(){
+System.register(["./index-legacy-6456c41c.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.m,i=e.l}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
