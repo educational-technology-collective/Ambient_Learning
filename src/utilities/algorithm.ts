17b78b92c370766428fb597bec1152c60a287d78
haytham918 <yunxuant@umich.edu>
2023-07-11 15:50:35 -0400

    Algorithm Version 8

--- src/utilities/algorithm.ts
+++ src/utilities/algorithm.ts
@@ -52,7 +52,7 @@ export const srsAlgorithm = (card : userCard) => {
 
 
 export const algorithmTester = (previous: any, evaluation: any) => {
-  let memFactor = 1.92, interval = 1;
+  let memFactor = 1.95, interval = 1;
     // Case When the card is not new
     if(previous !== null){
       if(evaluation.swipeResult === 'noMore'){
@@ -62,10 +62,15 @@ export const algorithmTester = (previous: any, evaluation: any) => {
       interval = previous.interval;
       let difference = evaluation.know - evaluation.forget;
       if(evaluation.swipeResult === 'know'){
-        memFactor += 0.1;
+        memFactor += 0.09;
+        if(evaluation.tapResult === 0){
+          memFactor -= 0.012;
+        }else if(evaluation.tapResult === 2){
+          memFactor -= 0.01;
+        }
         if(difference >= 3 && interval === 1){
-          interval = 1 + difference;
-          memFactor = Math.max(1.3, memFactor + difference * 0.15);
+          interval = 2 + difference;
+          memFactor = Math.max(1.3, memFactor + difference * 0.12);
           return {memFactor: memFactor, interval: interval};
         }
       }else if(evaluation.swipeResult === 'forget'){
@@ -75,7 +80,7 @@ export const algorithmTester = (previous: any, evaluation: any) => {
         }
         return {memFactor: memFactor, interval: 1};
       }else{
-        memFactor -= 0.05;
+        memFactor -= 0.005;
       }
       memFactor = Math.max(1.3, memFactor);
       interval = Math.ceil(previous.interval * memFactor);
