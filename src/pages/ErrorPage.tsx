3dba7419f7cfc858ba9b5bf3799e3a50404ac86b
haytham918 <yunxuant@umich.edu>
2023-07-18 12:06:50 -0400

    Working on buttons

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -2,6 +2,7 @@ import { IonButton, IonContent, IonPage, IonText, useIonViewWillEnter, useIonVie
 import './ErrorPage.css'
 import errorPic from "../../error.png"
 import { hideBar, showBar } from "../utilities/showTabBar";
+import LogOutButton from "../LogComp/LogOutButton";
 
 const ErrorPage : React.FC = () => {
   useIonViewWillEnter(hideBar)
@@ -12,7 +13,11 @@ const ErrorPage : React.FC = () => {
         <div className="error-container">
         <img src={errorPic} className='error-image'/>
         <IonText className="error-message">Ooops...<br></br>An Error Occured:( </IonText>
-        <button className="btn-grad">Try Again</button>
+        <div className="two-button-container">
+        <button className="try-again-button">Try Again</button>
+        <a className="report-issue-button">Report Issue</a>
+        </div>
+        <LogOutButton/>
         </div>
       </IonContent>
     </IonPage>
