1097b32cabc9bae9a9bc810ad798b1876c490112
haytham918 <yunxuant@umich.edu>
2023-07-24 15:44:54 -0400

    Review WalkingPerson

--- src/LoadingComp/WalkingPerson.tsx
+++ src/LoadingComp/WalkingPerson.tsx
@@ -2,6 +2,7 @@ import "./WalkingPerson.css";
 
 const WalkingPerson: React.FC = () => {
   return (
+    // Walking Person contains Head, Body, Two Legs, and Shadow
     <div className="walking">
       <div className="head"></div>
       <div className="body"></div>
