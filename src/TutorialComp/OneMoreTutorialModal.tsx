6153619b3ce8cb727b6df25cd125a3444d755f5a
haytham918 <yunxuant@umich.edu>
2023-07-24 15:25:34 -0400

    Review OneMoreTutorialModal

--- src/TutorialComp/OneMoreTutorialModal.tsx
+++ src/TutorialComp/OneMoreTutorialModal.tsx
@@ -5,6 +5,7 @@ const OneMoreTutorialModal: React.FC = () => {
   return (
     <div className="show-up">
       <img src={arrow} alt="Arrow" className="arrow-image" />
+      {/* Modal including the description text */}
       <div className="onemore-modal">
         <div className="onemore-modal-text">
           Number At Top-Right Indicates Number of Same Concept Cards
