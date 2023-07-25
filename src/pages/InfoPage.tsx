0342088bcdab73ae01fa5a9fd8e9dbfab32720da
haytham918 <yunxuant@umich.edu>
2023-07-25 12:49:16 -0400

    Edit InfoPage

--- src/pages/InfoPage.tsx
+++ src/pages/InfoPage.tsx
@@ -1,6 +1,11 @@
-import { IonContent, IonPage } from "@ionic/react";
-
+import { IonContent, IonPage, useIonViewWillEnter } from "@ionic/react";
+import './InfoPage.css'
+import { hideBar } from "../utilities/showTabBar";
 const InfoPage: React.FC = () => {
+
+  // Hide the bottom tabs whene entering the page
+  useIonViewWillEnter(hideBar);
+
   return(
     <IonPage>
       <IonContent scrollY={false} className="info-content">
