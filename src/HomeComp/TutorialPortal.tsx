1965c3cc98f8ac11a62b99e4cafd755278629299
haytham918 <yunxuant@umich.edu>
2023-07-26 10:24:35 -0400

    Add comemnts to TutorialPortal component

--- src/HomeComp/TutorialPortal.tsx
+++ src/HomeComp/TutorialPortal.tsx
@@ -1,29 +1,26 @@
-import { IonCard, IonCardContent, IonIcon, IonText } from '@ionic/react';
-import './TutorialPortal.css'
-import { diamond } from 'ionicons/icons';
-import { BiBookBookmark } from 'react-icons/bi';
-import { useHistory } from 'react-router';
+import { IonCard, IonCardContent, IonIcon, IonText } from "@ionic/react";
+import "./TutorialPortal.css";
+import { diamond } from "ionicons/icons";
+import { BiBookBookmark } from "react-icons/bi";
+import { useHistory } from "react-router";
 
 const TutorialPortal: React.FC = () => {
   const history = useHistory();
-  
+
   const navigateToTutorialScreen = () => {
     history.push("/tutorial");
   };
-  return(
+  return (
     <div className="top-container">
-    <IonIcon icon={diamond} style={{ fontSize: "3.5em" }}></IonIcon>
-    <IonCard
-      className="tutorial-card"
-      onClick={navigateToTutorialScreen}
-    >
-      <IonCardContent className="tutorial-card-content">
-        <BiBookBookmark size="2em" />
-        <IonText className="tutorial-text">Tutorial</IonText>
-      </IonCardContent>
-    </IonCard>
-  </div>
-  )
+      <IonIcon icon={diamond} style={{ fontSize: "3.5em" }}></IonIcon>
+      <IonCard className="tutorial-card" onClick={navigateToTutorialScreen}>
+        <IonCardContent className="tutorial-card-content">
+          <BiBookBookmark size="2em" />
+          <IonText className="tutorial-text">Tutorial</IonText>
+        </IonCardContent>
+      </IonCard>
+    </div>
+  );
 };
 
 export default TutorialPortal;
