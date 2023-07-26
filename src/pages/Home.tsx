f4e3160d3e2b1b7111eaea2f164c1e7b3ad852cd
haytham918 <yunxuant@umich.edu>
2023-07-26 10:23:05 -0400

    Remove unused stuff from home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,9 @@
-import {
-  IonContent,
-  IonPage,
-  IonIcon,
-  IonCard,
-  IonCardContent,
-  IonText,
-} from "@ionic/react";
+import { IonContent, IonPage } from "@ionic/react";
 import "./Home.css";
 import { useHistory } from "react-router-dom";
-import { diamond } from "ionicons/icons";
 import DashBoard from "../HomeComp/DashBoard";
 import AppNameHeader from "./AppNameHeader";
 import LogOutButton from "../ButtonComp/LogOutButton";
-import { BiBookBookmark } from "react-icons/bi";
 import { useAuth0 } from "@auth0/auth0-react";
 import TutorialPortal from "../HomeComp/TutorialPortal";
 
@@ -29,7 +20,6 @@ const Home: React.FC<{ cardsLeft: number; handleCardScreen: () => void }> = ({
     history.push("/cardscreen");
     handleCardScreen();
   };
-
 
   // Determine The Box-Shadow Effect based on cards remaining
   let shadow: string;
@@ -54,13 +44,11 @@ const Home: React.FC<{ cardsLeft: number; handleCardScreen: () => void }> = ({
 
       <IonContent scrollY={false} className="home-content">
         <div className="home-loaded-wrapper">
-
-        {/* Icon and Tutorial Portal */}
-         <TutorialPortal />
+          {/* Icon and Tutorial Portal */}
+          <TutorialPortal />
 
           {/* Welcome User */}
           <h1 className="user-name">Hey, {user?.name}</h1>
-
 
           {/* Dashbord of today's task */}
           <DashBoard
