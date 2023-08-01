4cc9ce416378e3d6ef7fcba6985b51c05cdcade0
haytham918 <yunxuant@umich.edu>
2023-08-01 12:39:52 -0400

    Change the appId

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'CHYT229TBS.com.etc.ambientlearning',
+  appId: 'com.etc.ambientlearning',
   appName: 'Ambient Learning',
   webDir: 'dist',
   server: {
