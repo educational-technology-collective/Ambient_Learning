6df4c7c9d9409712a7ded686393a313e72a0a24b
haytham918 <yunxuant@umich.edu>
2023-08-02 13:45:13 -0400

    Add a maple tree Icon and change applicationId for android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.etc.ambientlearning',
-  appName: 'Ambient',
+  appName: 'Ambient Learning',
   webDir: 'dist',
   server: {
     androidScheme: 'https',
