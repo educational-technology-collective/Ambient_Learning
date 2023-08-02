16aa01a921d7f4b6b899bc7a7f3098e127fee5a8
haytham918 <yunxuant@umich.edu>
2023-08-02 15:59:32 -0400

    new for android

--- dist/assets/swipe-back-legacy-5608c7a1.js
+++ dist/assets/swipe-back-legacy-5608c7a1.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-dadfc71a.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-240e483c.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
