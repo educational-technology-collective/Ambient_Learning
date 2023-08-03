f9dd9ea45ae0662ccb6e7fffa5121f76647a0893
haytham918 <yunxuant@umich.edu>
2023-08-03 10:51:36 -0400

    Change the login page

--- src/pages/LogInPage.tsx
+++ src/pages/LogInPage.tsx
@@ -3,6 +3,7 @@ import "./LogInPage.css";
 import LogInButton from "../ButtonComp/LogInButton";
 import { hideBar } from "../utilities/showTabBar";
 import spacecraft from "../../spacecraft.svg";
+import logo from '../../assets/logo.png'
 
 const LogInPage: React.FC = () => {
   // Hide the bottom tabs when entering the login page
@@ -12,21 +13,21 @@ const LogInPage: React.FC = () => {
     <IonPage>
       <IonContent scrollY={false} className="login-content">
         <div className="login-page-container">
-          <img src={spacecraft} alt="Spacecraft" className="space-craft" />
+          <img src={logo} alt="logo" className="login-logo" />
 
           {/* Welcome Text and the Rectangle Span */}
-          <div className="welcome-container">
+          {/* <div className="welcome-container">
             <h1 className="welcome-text">
               Hi, Welcome<span className="welcome-covering"></span>
             </h1>
-          </div>
+          </div> */}
 
           {/* Desciption Text and the Login Button appears with changing opacity */}
-          <div>
+          {/* <div>
             <h2 className="description-text">
               Enlarge Brain Through <i>Flashcards</i>
             </h2>
-          </div>
+          </div> */}
           <LogInButton />
         </div>
       </IonContent>
