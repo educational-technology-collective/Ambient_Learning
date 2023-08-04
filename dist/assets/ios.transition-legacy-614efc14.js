1f3204f6d0b68b2e33f3c92f50569f77426d6f5f
haytham918 <yunxuant@umich.edu>
2023-08-04 10:51:58 -0400

    Update ios version

--- dist/assets/ios.transition-legacy-614efc14.js
+++ dist/assets/ios.transition-legacy-614efc14.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-7a043435.js"],(function(e,t){"use strict";var o,n;return{setters:[e=>{o=e.l,n=e.m}],execute:function(){
+System.register(["./index-legacy-a42b38c8.js"],(function(e,t){"use strict";var o,n;return{setters:[e=>{o=e.l,n=e.m}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
