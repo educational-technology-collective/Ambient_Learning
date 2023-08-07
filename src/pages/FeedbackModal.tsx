a4240fdbae7840cff33840f10d3b808ec223f419
haytham918 <yunxuant@umich.edu>
2023-08-07 11:33:18 -0400

    Make feedback working

--- src/pages/FeedbackModal.tsx
+++ src/pages/FeedbackModal.tsx
@@ -1,60 +1,76 @@
 import { useRef } from "react";
-import './FeedbackModal.css'
-import {ImCross} from 'react-icons/im'
+import "./FeedbackModal.css";
+import { ImCross } from "react-icons/im";
 import { CapacitorHttp } from "@capacitor/core";
 import { useAuth0 } from "@auth0/auth0-react";
 
-const FeedbackModal: React.FC<{identifier: string, closeQuestion: () => void}> = ({identifier, closeQuestion}) => {
-
+const FeedbackModal: React.FC<{
+  identifier: string;
+  closeQuestion: () => void;
+}> = ({ identifier, closeQuestion }) => {
   const form = useRef(null);
 
-  const {getAccessTokenSilently} = useAuth0();
+  const { getAccessTokenSilently } = useAuth0();
 
   const postFeedback = async (title: string, content: string) => {
-     const accessToken = await getAccessTokenSilently();
+    const accessToken = await getAccessTokenSilently();
     const body = {
-     title: title,
-     identifier: identifier,
-     content: content
-    }
-    const response = await CapacitorHttp.post({url: 'https://a97mj46gc1.execute-api.us-east-1.amazonaws.com/spaced-rep-dev-feedback-POST', data: body, headers: {
-      "content-type": "application/json",
-      authorization: `Bearer ${accessToken}`,
-    }});
-    console.log('Post Feedback', response);
-  }
+      title: title,
+      identifier: identifier,
+      content: content,
+    };
+    const response = await CapacitorHttp.post({
+      url: "https://a97mj46gc1.execute-api.us-east-1.amazonaws.com/dev/feedback",
+      data: body,
+      headers: {
+        "content-type": "application/json",
+        authorization: `Bearer ${accessToken}`,
+      },
+    });
+    console.log("Post Feedback", response);
+  };
 
-  const sendFeedback = (e : any) => {
+  const sendFeedback = (e: any) => {
     e.preventDefault();
-    if(e.target[0].value !== '' && e.target[1].value !== ''){
-       postFeedback(e.target[0].value, e.target[1].value);
-       closeQuestion();
-    }else{
-      alert('Make sure you fill both fields');
+    if (e.target[0].value !== "" && e.target[1].value !== "") {
+      postFeedback(e.target[0].value, e.target[1].value);
+      closeQuestion();
+    } else {
+      alert("Make sure you fill both fields");
     }
-  }
- console.log('Feedbakcform')
-  return(
+  };
+  console.log("Feedbakcform");
+  return (
     <div className="contact contact__container container grid">
-        <a onClick={closeQuestion} className="close-icon"><ImCross size='2.5rem'/></a>
-        <form ref={form} onSubmit={sendFeedback} className="content__form">
-            <div className="contact__form-div">
-              <label className="contact__form-tag">Title</label>
-              <input type="text" name='name' className="contact__form-input" placeholder="What's it about"/>
-            </div>
+      <a onClick={closeQuestion} className="close-icon">
+        <ImCross size="2.5rem" />
+      </a>
+      <form ref={form} onSubmit={sendFeedback} className="content__form">
+        <div className="contact__form-div">
+          <label className="contact__form-tag">Title</label>
+          <input
+            type="text"
+            name="name"
+            className="contact__form-input"
+            placeholder="What's it about"
+          />
+        </div>
 
-            <div className="contact__form-div contact__form-area">
-              <label className="contact__form-tag">Content</label>
-              <textarea name='message' cols={30} rows={10} className="contact__form-input" placeholder="Leave Message"></textarea>
-            </div>
+        <div className="contact__form-div contact__form-area">
+          <label className="contact__form-tag">Content</label>
+          <textarea
+            name="message"
+            cols={30}
+            rows={10}
+            className="contact__form-input"
+            placeholder="Leave Message"
+          ></textarea>
+        </div>
 
-            <button  className="button-message button--flex">
-          Send Message 
-          
-        </button>
-          </form>
+        <button className="button-message button--flex">Send Message</button>
+      </form>
     </div>
-  )
+  );
 };
 
 export default FeedbackModal;
