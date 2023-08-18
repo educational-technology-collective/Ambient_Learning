c8430da4cbf77a9b853fac5199bd22c5e155bb12
haytham918 <yunxuant@umich.edu>
2023-08-18 15:07:10 -0400

    Change target sdk to 33 for android

--- dist/assets/ios.transition-legacy-75f1e299.js
+++ dist/assets/ios.transition-legacy-75f1e299.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-bc357df5.js"],(function(e,t){"use strict";var o,n;return{setters:[e=>{o=e.l,n=e.m}],execute:function(){
+System.register(["./index-legacy-47bd8e34.js"],(function(e,t){"use strict";var o,n;return{setters:[e=>{o=e.l,n=e.m}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
--- dist/assets/swipe-back-legacy-4661e945.js
+++ dist/assets/swipe-back-legacy-4661e945.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-bc357df5.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-47bd8e34.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
