945412ef8d8a975a5b8b1ddaae79ecdc0c900971
haytham918 <yunxuant@umich.edu>
2023-09-06 11:30:47 -0400

    Adjust the cards tab fontWeight and position

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {VitePWA} from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    // react(),
     VitePWA({ registerType: 'autoUpdate' }),
     legacy()
   ],
