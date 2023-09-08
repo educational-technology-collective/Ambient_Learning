6234121879e7bd2e1d2a4d096b7176c2ed362731
haytham918 <yunxuant@umich.edu>
2023-09-08 11:07:13 -0400

    Live Update Commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,9 @@ import {VitePWA} from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    // react(),
-    VitePWA({ registerType: 'autoUpdate' }),
-    legacy()
+  //  react(),
+ //   VitePWA({ registerType: 'autoUpdate' }),
+  //  legacy()
   ],
   test: {
     globals: true,
