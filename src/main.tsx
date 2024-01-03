aa584f0ab0e16b4f9cfeee8b15c9bd32212aa737
Luke Anger <112991266+LukeAnger@users.noreply.github.com>
2024-01-03 10:10:30 -0500

    config updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
-import "preact/debug";
-import "preact/devtools";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Auth0Provider } from "@auth0/auth0-react";
-import App from "./App";
+import App from "./App.jsx";
 import { isPlatform } from "@ionic/react";
 
 const container = document.getElementById("root");
